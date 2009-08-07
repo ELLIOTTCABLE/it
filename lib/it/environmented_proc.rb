6341b51f96c9b737a16d7fb84112df2bef4f02b5
elliottcable <git@elliottcable.name>
2009-08-06 22:51:58 -0800

    Variables are now defined on the block before execution and undefined afterwards

--- lib/it/environmented_proc.rb
+++ lib/it/environmented_proc.rb
@@ -3,6 +3,9 @@ class EnvironmentedProc < Proc
   attr_accessor :self
   def self; @self ||= binding.eval("self"); end
   
+  attr_accessor :variables
+  def variables; @variables ||= Hash.new; end
+  
   def initialize &block
     raise ArgumentError, 'EnvironmentedProcs may not have arguments' unless block.arity.zero?
     super
@@ -16,9 +19,7 @@ class EnvironmentedProc < Proc
   end
   
   def inject variables
-    variables.map do |variable, object|
-      (class<<@self;self;end).send(:define_method, variable) {object}
-    end
+    self.variables.merge! variables
     return self
   end
   Speck.new EnvironmentedProc.instance_method :inject do
@@ -45,7 +46,21 @@ class EnvironmentedProc < Proc
   end
   
   def call
-    @self.instance_eval &self
+    eigenclass = class<<@self;self;end
+    reimplementables = variables.map do |variable, object|
+      umethod = begin eigenclass.instance_method variable; rescue NameError; nil; end
+      eigenclass.send(:define_method, variable) {object}
+      [variable, umethod]
+    end
+    
+    rv = @self.instance_eval &self
+    
+    reimplementables.each do |variable, umethod|
+      umethod ? eigenclass.send(:define_method, variable, &umethod) :
+        eigenclass.send(:remove_method, variable)
+    end
+    
+    return rv
   end
   Speck.new EnvironmentedProc.instance_method :call do
     object = Object.new
